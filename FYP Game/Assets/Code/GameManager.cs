fe51bfd8fdfe9bfcfd88b943582b2ddf761d1740
TahmidTarique <145116011+MistBreath@users.noreply.github.com>
2025-12-18 20:11:30 +0000

    need to add timer to 2nd button

--- FYP Game/Assets/Code/GameManager.cs
+++ FYP Game/Assets/Code/GameManager.cs
@@ -7,15 +7,42 @@ public class GameManager : MonoBehaviour
 {
     public TMP_Text countText;
 
-    int count = 0;
+    public int count = 0;
+    int count2 = 0;
+    float nextTimecheck = 1;
     void Start()
     {
         UpdateUI();
+    }
+    void Update()
+    {
+        if (nextTimecheck < Time.timeSinceLevelLoad)
+        {
+            IdleCalculate();
+            nextTimecheck = Time.timeSinceLevelLoad + 1f;
+        }
+        
+        
+    }
+    void IdleCalculate()
+    {
+
+    }
+    public bool buyAction(int cost)
+    {
+        if (count >= cost)
+        {
+            count -= cost;
+            UpdateUI();
+            return true;
+        }
+        return false;
     }
     public void ClickAction()
     {
         Debug.Log("CLICK!!!!");
         count++;
+        Debug.Log(count);
         UpdateUI();
         
     }
--- FYP Game/Assets/Code/Upgrades.cs
+++ FYP Game/Assets/Code/Upgrades.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Upgrades : MonoBehaviour
@@ -6,27 +7,66 @@ public class Upgrades : MonoBehaviour
     [Header("Components")]
     public TMP_Text countText;
     public TMP_Text incomeInfoText;
-    public int startNumber = 15;
+    public TMP_Text percentageInfoText;
+    public int startNumber = 5;
     public float upgradeNumberMultiplier;
-    public float NumberPerUpgrade = 0.1f;
-
+    public float NumberPerUpgrade = 1f;
+    [Header("Managers")]
+    public GameManager gameManager;
+    [Header("Generator values")]
     int level = 0;
+
+    private void Awake()
+    {
+        Debug.Log("countText = " + countText);
+        Debug.Log("incomeInfoText = " + incomeInfoText);
+        Debug.Log("gameManager = " + gameManager);
+    }
 
     private void Start()
     {
-        UpdateUI();
+       UpdateUI();
+    }
+    public void ClickAction()
+    {
+        Canvas c = FindAnyObjectByType<Canvas>();
+       
+
+        //Canvas c = GetComponentInParent<Canvas>();
+        GameManager gm = c.GetComponent<GameManager>();
+
+        int price = CalculateNumber();
+        bool buySuccess = gm.buyAction(price); // gameManager.buyAction(price);
+        if (buySuccess)
+        {
+            level++;
+            UpdateUI();
+        }
     }
 
     void UpdateUI()
     {
         countText.text = CalculateNumber().ToString();
+        percentageInfoText.text = incomeInfoText.text;
         incomeInfoText.text = level.ToString() + " x " + NumberPerUpgrade + "/s";
+
+        //if(time_since_last_update > 1000)
+        //{
+        //    Canvas c = FindAnyObjectByType<Canvas>();
+
+        //    GameManager gm = c.GetComponent<GameManager>();
+
+        //    gm.count = gm.count + level;
+
+        //    time_since_last_updte = now;
+        //}
         // 5 x 0.5/s
     }
 
     int CalculateNumber()
     {
         int number = Mathf.RoundToInt(startNumber * Mathf.Pow(upgradeNumberMultiplier, level));
+        Debug.Log(number);
         return number;
     }
     public float CalculateIncomePerSecond()
